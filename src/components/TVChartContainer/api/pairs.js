0632d8da7e3e7aff1432eaf6253bb4a4ddf89e5f
Oleg Bohdan <nero232@ukr.net>
2020-12-28 12:21:22 +0200

    Update pairs.js

--- src/components/TVChartContainer/api/pairs.js
+++ src/components/TVChartContainer/api/pairs.js
@@ -1,45 +1,30 @@
 export default {
-  "BTC/USD": "Coinbase",
-  "ETH/USD": "Coinbase",
-  "XLM/BTC": "Bitfinex",
-  "XLM/ETH": "Bitfinex",
-  "XLM/USD": "Bitfinex",
+  "BTCUSDTPERP": "Binance",  
+  "BCHUSDTPERP": "Binance",  
+  "ETHUSDTPERP": "Binance"
 }
 
 export const data = [
 	{
-		symbol: 'BTC/USD',
-		full_name: 'BTC/USD',
+		symbol: 'BTCUSDTPERP',
+		full_name: 'BTCUSDTPERP',
 		description: 'description',
-		exchange: 'Coinbase',
+		exchange: 'Binance',
 		type: 'type'
 	},
 	{
-		symbol: 'ETH/USD',
-		full_name: 'ETH/USD',
+	    symbol: 'ETHUSDTPERP',
+		full_name: 'ETHUSDTPERP',
 		description: 'description',
-		exchange: 'Coinbase',
+		exchange: 'Binance',
 		type: 'type'
 	},
 	{
-		symbol: 'XLM/BTC',
-		full_name: 'XLM/BTC',
+		symbol: 'BCHUSDTPERP',
+		full_name: 'BCHUSDTPERP',
 		description: 'description',
-		exchange: 'Bitfinex',
-		type: 'type'
-	},
-	{
-		symbol: 'XLM/ETH',
-		full_name: 'XLM/ETH',
-		description: 'description',
-		exchange: 'Bitfinex',
-		type: 'type'
-	},
-	{
-		symbol: 'XLM/USD',
-		full_name: 'XLM/USD',
-		description: 'description',
-		exchange: 'Bitfinex',
+		exchange: 'Binance',
 		type: 'type'
 	}
+	 
 ]
